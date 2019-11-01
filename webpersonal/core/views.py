ad8f2add1dc5ad83b9a88e8f20519519813d6373
LaRosaM <jose.larosa6@unmsm.edu.pe>
2019-11-01 14:14:14 -0500

    Vista Home básica implementada

--- webpersonal/core/views.py
+++ webpersonal/core/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
-# Create your views here.
+
+def home(request):
+    html_response = '<h1>Mi web personal</h1>'
+    for i in range(10):
+        html_response += '<h2>Portada</h2>'
+    return HttpResponse(html_response)
