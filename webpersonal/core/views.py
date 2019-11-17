4e8e667a5fb24016e8679c261fbd96e445f2a559
LaRosaM <jose.larosa6@unmsm.edu.pe>
2019-11-17 02:37:51 -0500

    Video 14

--- webpersonal/core/views.py
+++ webpersonal/core/views.py
@@ -1,39 +1,17 @@
 from django.shortcuts import render, HttpResponse
 
-html_base = """
-<h1>Mi web personal</h1>
-<ul>
-    <li><a href="/">Portada</a></li>
-    <li><a href="/about-me">Acerca de</a></li>
-    <li><a href="/portafolio">Portafolio</a></li>
-    <li><a href="/contacto">Contacto</a></li>
-</ul>
-"""
-
 
 def home(request):
-    return HttpResponse(html_base + """
-        <h2>Portada</h2>
-        <p>Esto es la portada</p>
-    """)
+    return render(request, "core/home.html")
 
 
 def about(request):
-    return HttpResponse(html_base + """
-        <h2>Acerca de</h2>
-        <p>Me llamo José y soy programador</p>
-    """)
+    return render(request, "core/about.html")
 
 
 def portafolio(request):
-    return HttpResponse(html_base + """
-        <h2>Portafolio</h2>
-        <p>Este es mi portafolio</p>
-    """)
+    return render(request, "core/portafolio.html")
 
 
 def contacto(request):
-    return HttpResponse(html_base + """
-        <h2>Contacto</h2>
-        <p>Mi contacto es el jose.larosa6@unmsm.edu.pe</p>
-    """)
+    return render(request, "core/contacto.html")
